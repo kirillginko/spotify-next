8b4f61a507436e8e2f45c9effa8267bf90b67bce
Kirill Ginko <kirillginko@gmail.com>
2021-12-06 21:42:43 -0500

    added sidebar and main playlists

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,10 @@
+import { atom } from "recoil";
+
+export const playlistIdState = atom({
+  key: "playlistIdState",
+  default: "0o7JNSsc0m9dhCnrG1DU9N",
+});
+export const playlistState = atom({
+  key: "playlistState",
+  default: null,
+});
